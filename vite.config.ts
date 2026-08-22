3a1b96d568bbea8d49ba39d3efba035014c6c9e3
nobschulth <nbsfone@gmail.com>
2026-08-22 13:18:29 +0200

    Added a missing comma...

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     plugins: [react()], 
     server: {
         allowedHosts: ["holophytic-agedly-katherin.ngrok-free.dev"]
-    }
+    },
     base: "/REPOSITORY/",
 })
